200db7f79eee6cb632fccfec24c093d5619e0a05
Alexey Maringos <maringos.alexey@gmail.com>
2026-04-30 10:20:30 +0300

    chore: добавлен setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vex",
+    version="0.2.0",
+    description="Version generator for make/cmake projects",
+    packages=find_packages(),
+    python_requires=">=3.10",
+    entry_points={
+        "console_scripts": [
+            "vex=vex.cli:main",
+        ],
+    },
+)
