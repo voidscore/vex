e0e8a6d667952a7559bb570ad93fb9a252fd2f18
Alexey Maringos <maringos.alexey@gmail.com>
2026-04-30 12:29:25 +0300

    chore(version): bump to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vex",
-    version="0.2.2",
+    version="0.3.0",
     description="Version generator for make/cmake projects",
     packages=find_packages(),
     python_requires=">=3.10",
